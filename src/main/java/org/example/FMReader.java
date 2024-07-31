42c8d737496a02655467dfc48dfed8f956f9047a
Max <99667961+maxbuh@users.noreply.github.com>
2024-08-01 01:21:12 +0200

    Gute Güte, jetzt funktioniert es wirklich
    Alternativ Gruppen gefixed

--- src/main/java/org/example/FMReader.java
+++ src/main/java/org/example/FMReader.java
@@ -22,28 +22,27 @@ public class FMReader {
 
     public static void main(String[] args) throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
         FMReader reader = new FMReader();
-        String modelPath = "src/main/modelle/excludeModel.xml";
+        String modelPath = "src/main/modelle/J2_T10_M2_O2_A4.xml";
         reader.ReadFM(modelPath);
 
 
         System.out.println("\n" + "Erstellte Jobs:");
-        for(int i = 0; i < reader.jobs.size(); i++) {
+        for (int i = 0; i < reader.jobs.size(); i++) {
             System.out.println("\n Job " + i + ": ");
-            for(Task task : reader.jobs.get(i)) {
+            for (Task task : reader.jobs.get(i)) {
                 System.out.print(task.name + "  [" + task.duration[0] + "," + task.duration[1] + "]  " + task.machine + "  " + task.optional + "  |  ");
-                for(String s : task.excludeTasks) {
+                for (String s : task.excludeTasks) {
                     System.out.print(" " + s);
                 }
             }
         }
-        for(int i = 0; i < reader.machines.size(); i++) {
+        for (int i = 0; i < reader.machines.size(); i++) {
             System.out.print("\n Machine " + i + ": " + reader.machines.get(i).name + "  " + reader.machines.get(i).id + "  " + reader.machines.get(i).optional);
         }
 
 
-
-
     }
+
     public void ReadFM(String modelPath) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
         String modellpfad = modelPath;
 
@@ -85,9 +84,9 @@ public class FMReader {
 
         int id = 0;
 
-        for(int i = 0; i < machineNodes.getLength(); i++) {
-            if(machineNodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                if(machineNodes.item(i).getAttributes().getLength() == 2) {
+        for (int i = 0; i < machineNodes.getLength(); i++) {
+            if (machineNodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                if (machineNodes.item(i).getAttributes().getLength() == 2) {
                     Machine machine = new Machine(id, false);
                     machine.name = machineNodes.item(i).getAttributes().getNamedItem("name").getNodeValue();
                     id++;
@@ -109,11 +108,11 @@ public class FMReader {
 
         NodeList taskNodes = p.getChildNodes();
 
-        for(int i = 0; i < taskNodes.getLength(); i++) {
+        for (int i = 0; i < taskNodes.getLength(); i++) {
             Node currentNode = taskNodes.item(i);
-            if(currentNode.getNodeType() == Node.ELEMENT_NODE) {
+            if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
                 Task task = new Task();
-                if(currentNode.getAttributes().getLength() == 2) {
+                if (currentNode.getAttributes().getLength() == 2) {
                     task.name = currentNode.getAttributes().item(1).getNodeValue();
                     task.optional = false;
                 } else {
@@ -127,15 +126,15 @@ public class FMReader {
                 int durationsArrIndex = 0;
                 boolean durationsAlreadySet = true;
                 List<Integer> durationsList = new ArrayList<>();
-                for(int j = 0; j < durations.getLength(); j++) {
+                for (int j = 0; j < durations.getLength(); j++) {
                     Node currentDuration = durations.item(j);
-                    if(currentDuration.getNodeType() == Node.ELEMENT_NODE) {
-                        if(currentDuration.getAttributes().getLength() == 2) {
+                    if (currentDuration.getNodeType() == Node.ELEMENT_NODE) {
+                        if (currentDuration.getAttributes().getLength() == 2) {
                             String durationString = currentDuration.getAttributes().item(1).getNodeValue();
                             String[] subStrings = durationString.split(" ");
                             durationsArr[0] = Integer.parseInt(subStrings[2]);
                             durationsArr[1] = Integer.parseInt(subStrings[2]);
-                        } else if(currentDuration.getAttributes().getLength() == 1) {
+                        } else if (currentDuration.getAttributes().getLength() == 1) {
                             durationsAlreadySet = false;
                             String durationString = currentDuration.getAttributes().item(0).getNodeValue();
                             String[] subStrings = durationString.split(" ");
@@ -145,7 +144,7 @@ public class FMReader {
                         }
                     }
                 }
-                if(!durationsAlreadySet) {
+                if (!durationsAlreadySet) {
                     Collections.sort(durationsList);
                     durationsArr[0] = durationsList.get(0);
                     durationsArr[1] = durationsList.get(durationsList.size() - 1);
@@ -164,22 +163,22 @@ public class FMReader {
 
         List<String[]> orderConstraints = new ArrayList<>();
         List<String[]> machineConstraints = new ArrayList<>();
-        for(int i = 0; i < constraints.getLength(); i++) {
+        for (int i = 0; i < constraints.getLength(); i++) {
             Node currentConstraint = constraints.item(i);
             NodeList constraintPair = currentConstraint.getChildNodes();
 
             String[] constraintPairArr = new String[2];
 
             int index = 0;
-            for(int j = 0; j < constraintPair.getLength(); j++) {
-                if(constraintPair.item(j).getNodeType() == Node.ELEMENT_NODE) {
+            for (int j = 0; j < constraintPair.getLength(); j++) {
+                if (constraintPair.item(j).getNodeType() == Node.ELEMENT_NODE) {
                     constraintPairArr[index] = constraintPair.item(j).getTextContent();
                     index++;
                 }
             }
 
             // Zuordnen ob es eine Reihenfolge-Constraint oder eine Machine-Constraint ist
-            if(constraintPairArr[1].startsWith("m")) {
+            if (constraintPairArr[1].startsWith("m")) {
                 machineConstraints.add(constraintPairArr);
             } else {
                 orderConstraints.add(constraintPairArr);
@@ -192,14 +191,14 @@ public class FMReader {
         // Die tasks, die in der orderList bei keinem Orderpaar an erster Stelle stehen, sind Starttasks
         // Zuerst alle Tasks die bei den Orderpaaren an erster Stelle stehen in notStarter einfügen
         Set<Task> notStarter = new HashSet<>();
-        for(int i = 0; i < orderConstraints.size(); i++) {
+        for (int i = 0; i < orderConstraints.size(); i++) {
             int finalI = i;
             Task task = allTasks.stream()
                     .filter(t -> orderConstraints.get(finalI)[0].equals(t.name))
                     .findAny()
                     .orElse(null);
 
-            if(task != null) {
+            if (task != null) {
                 notStarter.add(task);
             }
         }
@@ -207,6 +206,24 @@ public class FMReader {
         //Startertasks sind die Differenz von unusedTasks \ notStarter
         unusedTasks.removeAll(notStarter);
 
+        for (Task task : unusedTasks) {
+            List<Task> job = new ArrayList<>();
+            job.add(task);
+            String currentTaskName = task.name;
+            for (int i = 0; i < orderConstraints.size(); i++) {
+                String[] currentConstraint = orderConstraints.get(i);
+                if (orderConstraints.get(i)[1].equals(currentTaskName)) {
+                    Task followTask = taskNameMap.get(orderConstraints.get(i)[0]);
+                    job.add(followTask);
+                    currentTaskName = orderConstraints.get(i)[0];
+                    orderConstraints.remove(orderConstraints.get(i));
+                    i = -1;
+
+                }
+            }
+            jobs.add(job);
+        }
+        /*
         //unusedTasks enthält jetzt nurnoch Tasks die auch Startertasks sind
         //Für jede unusedTask einen Job erstellen
         for(int i = 0; i < unusedTasks.size(); i++) {
@@ -234,6 +251,8 @@ public class FMReader {
                         tempPreviousTask = task.name;
                         notStarter.remove(task);
                     }
+                    orderConstraints.remove(j);
+                    j = 0;
                 }
             }
 
@@ -263,6 +282,8 @@ public class FMReader {
 
             jobs.add(job);
         }
+        /*
+
         // ==============================
         // Machine Constraints
         // ==============================
@@ -280,7 +301,7 @@ public class FMReader {
             System.out.println(pair[0] + "  " + pair[1]);
         }*/
 
-        for(String[] con : machineConstraints) {
+        for (String[] con : machineConstraints) {
             // Maschine Finden die den selben Namen hat wie die Maschine in der Constraint
             Machine machine = machines.stream()
                     .filter(mach -> con[1].equals(mach.name))
@@ -291,7 +312,7 @@ public class FMReader {
                     .filter(t -> con[0].equals(t.name))
                     .findAny()
                     .orElse(null);
-            if((machine != null) && (task != null)) {
+            if ((machine != null) && (task != null)) {
                 task.machine = machine.id;
             }
         }
@@ -299,8 +320,76 @@ public class FMReader {
         // ==============================
         // Exclude Constraints
         // ==============================
-        int amount = exclConstraints.getLength();
+        int i = 1;
+        String expression = "(//rule/disj/conj)[" + i + "]//text()";
+        Object evaluation = xPath.compile(expression).evaluate(modellDoc, XPathConstants.NODESET);
+        List<String> taskNames = new ArrayList<>();
+        if (evaluation instanceof NodeList) {
+            NodeList list = (NodeList) evaluation;
+            for (int j = 0; j < list.getLength(); j++) {
+                String text = list.item(j).getNodeValue().trim();
+                if (!text.isEmpty()) {
+                    System.out.println(text);
+                    taskNames.add(text);
+                }
+            }
+        }
+        int listSize = taskNames.size();
+        List<String> prevList = new ArrayList<>();
 
+        while (listSize > 0) {
+            i++;
+            boolean sameList = false;
+            if ((prevList != null) && (taskNames.size() == prevList.size())) {
+                sameList = true;
+                for (int j = 0; j < taskNames.size(); j++) {
+                    if (!prevList.contains(taskNames.get(j))) {
+                        sameList = false;
+                        break;
+                    }
+
+                }
+            }
+            if (!sameList) {
+                String[] names = new String[taskNames.size()];
+                names = taskNames.toArray(names);
+
+                for (int j = 0; j < names.length; j++) {
+                    Task task = taskNameMap.get(names[j]);
+
+                    for (int k = 0; k < names.length; k++) {
+                        if (k != j) {
+                            task.excludeTasks.add(names[k]);
+                        }
+                    }
+                }
+            }
+            if (prevList != null) {
+                prevList.clear();
+            }
+            for(String n : taskNames) {
+                prevList.add(n);
+            }
+
+            String expression2 = "(//rule/disj/conj)[" + i + "]//text()";
+            Object evaluation2 = xPath.compile(expression2).evaluate(modellDoc, XPathConstants.NODESET);
+            taskNames.clear();
+            if (evaluation2 instanceof NodeList) {
+                NodeList list = (NodeList) evaluation2;
+                for (int j = 0; j < list.getLength(); j++) {
+                    String text = list.item(j).getNodeValue().trim();
+                    if (!text.isEmpty()) {
+                        System.out.println(text);
+                        taskNames.add(text);
+                    }
+                }
+            }
+            listSize = taskNames.size();
+        }
+
+
+
+            /*
         for(int i = 1; i <= amount; i++) {
             String expression = "(//rule/disj/conj)[" + i + "]//text()";
 
@@ -318,7 +407,7 @@ public class FMReader {
                     }
                 }
             }
-
+            String taskNamesS = taskNames.toString();
             String[] names = new String[taskNames.size()];
             names = taskNames.toArray(names);
 
@@ -331,6 +420,7 @@ public class FMReader {
                     }
                 }
             }
-        }
+
+             */
     }
 }

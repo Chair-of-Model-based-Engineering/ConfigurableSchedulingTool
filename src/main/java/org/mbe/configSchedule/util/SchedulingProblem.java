6a0ddece0336b821e048fd5f607f3f008bd90033
Mathis Weiß <mathis.weiss@uni-siegen.de>
2025-01-15 08:54:53 +0100

    Comments for util

--- src/main/java/org/mbe/configSchedule/util/SchedulingProblem.java
+++ src/main/java/org/mbe/configSchedule/util/SchedulingProblem.java
@@ -14,38 +14,80 @@ public class SchedulingProblem implements Serializable {
     private List<Machine> machines;
     private int deadline;
 
+    /**
+     * Creates new object of type SchedulingProblem.
+     *
+     * @param jobs     a {@link List} of {@link List} of {@link Task Tasks}.
+     * @param machines a {@link List} of {@link Machine Machines}.
+     * @param deadline deadline of the scheduling problem.
+     */
     public SchedulingProblem(List<List<Task>> jobs, List<Machine> machines, int deadline) {
         this.jobs = jobs;
         this.machines = machines;
         this.deadline = deadline;
     }
 
+    /**
+     * Creates new objrct of type Scheduling problem from a feature model.
+     *
+     * @param featureModel parsed feature model.
+     */
     public SchedulingProblem(FeatureModel featureModel) {
         this.deadline = UVLReader.parseDeadline(featureModel);
         this.machines = UVLReader.parseMachines(featureModel);
         this.jobs = UVLReader.parseJobs(featureModel, machines);
     }
 
+    /**
+     * Get jobs of scheduling problem.
+     *
+     * @return a {@link List} of {@link List} of {@link Task Tasks}.
+     */
     public List<List<Task>> getJobs() {
         return jobs;
     }
 
+    /**
+     * Set jobs of scheduling problem.
+     *
+     * @param jobs a {@link List} of {@link List} of {@link Task Tasks}.
+     */
     public void setJobs(List<List<Task>> jobs) {
         this.jobs = jobs;
     }
 
+    /**
+     * Get machines of scheduling problem.
+     *
+     * @return {@link List} of {@link Machine Machines}.
+     */
     public List<Machine> getMachines() {
         return machines;
     }
 
+    /**
+     * Set machines of scheduling problem.
+     *
+     * @param machines {@link List} of {@link Machine Machines}.
+     */
     public void setMachines(List<Machine> machines) {
         this.machines = machines;
     }
 
+    /**
+     * Get deadline of scheduling problem.
+     *
+     * @return deadline as an integer.
+     */
     public int getDeadline() {
         return deadline;
     }
 
+    /**
+     * Set deadline of scheduling Problem.
+     *
+     * @param deadline new deadline.
+     */
     public void setDeadline(int deadline) {
         this.deadline = deadline;
     }
--- src/main/java/org/mbe/configSchedule/util/SolverReturn.java
+++ src/main/java/org/mbe/configSchedule/util/SolverReturn.java
@@ -12,6 +12,14 @@ public class SolverReturn {
     private String output;
     private Map<Machine, List<AssignedTask>> assignedJobs;
 
+    /**
+     * Creates new object of type SolverReturn.
+     *
+     * @param time         time it took to solve the problem.
+     * @param status       status of the solver.
+     * @param output       String output for display.
+     * @param assignedJobs Jobs executed in solution.
+     */
     public SolverReturn(Double time, CpSolverStatus status, String output, Map<Machine, List<AssignedTask>> assignedJobs) {
         this.time = time;
         this.status = status;
@@ -19,39 +27,80 @@ public class SolverReturn {
         this.assignedJobs = assignedJobs;
     }
 
+    /**
+     * Create new empty object of type SolverReturn.
+     */
     public SolverReturn() {
     }
 
-    ;
-
+    /**
+     * Get time spend to solve the problem.
+     *
+     * @return time in ms.
+     */
     public Double getTime() {
         return time;
     }
 
+    /**
+     * Set time spend to solve the problem.
+     *
+     * @param time new time in ms.
+     */
     public void setTime(Double time) {
         this.time = time;
     }
 
+    /**
+     * Get status of solver.
+     *
+     * @return status of solver.
+     */
     public CpSolverStatus getStatus() {
         return status;
     }
 
+    /**
+     * Sets status of solver.
+     *
+     * @param status new status of solver.
+     */
     public void setStatus(CpSolverStatus status) {
         this.status = status;
     }
 
+    /**
+     * Get output String.
+     *
+     * @return output String.
+     */
     public String getOutput() {
         return output;
     }
 
+    /**
+     * Set output String.
+     *
+     * @param output output String.
+     */
     public void setOutput(String output) {
         this.output = output;
     }
 
+    /**
+     * Get all jobs assigned to each machine in solution.
+     *
+     * @return all assigned jobs.
+     */
     public Map<Machine, List<AssignedTask>> getAssignedJobs() {
         return assignedJobs;
     }
 
+    /**
+     * Set Jobs assigned to each machine.
+     *
+     * @param assignedJobs new assigned jobs.
+     */
     public void setAssignedJobs(Map<Machine, List<AssignedTask>> assignedJobs) {
         this.assignedJobs = assignedJobs;
     }
